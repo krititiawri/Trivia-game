b3057b0993d8b5460757add88f08354c01041a83
krititiawri <kriit.tiwari28@gmail.com>
2017-07-19 05:07:27 -0500

    Changes made

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
+// Declaring array of object to print quiz question and answers
+var audio = new Audio("assests/songs/song.mp3");
+    audio.play();
+var clickSound = new Audio("assests/songs/click.mp3");    
 var Quiz = {
-	"data": [
-	  {
-	  	question:"what is captital of India",
-	  	options:["Delhi","shimla","calcutta","punjab"],
-	  	ans:"Delhi",
+  "data": [
+    {
+      question:"what is captital of India",
+      options:["Delhi","shimla","calcutta","punjab"],
+      ans:"Delhi",
       images: "assests/images/Delhi.gif",
-	  },
-	   {
-	  	question:"what is captital of United States Of America",
-	  	options:["Texas","California","Washington","Arizona"],
-	  	ans:"Washington",
+    },
+     {
+      question:"what is captital of United States Of America",
+      options:["Texas","California","Washington","Arizona"],
+      ans:"Washington",
       images:"assests/images/wash.gif",
-	  },
-	  {
-	  	question:"what is captital of United Kingdom",
-	  	options:["London","England","Scotland","Wales"],
-	  	ans:"London",
-	  }
-		]
+    },
+    {
+      question:"what is captital of United Kingdom",
+      options:["London","England","Scotland","Wales"],
+      ans:"London",
+      images:"assests/images/London-GIF.gif"
+    }
+    ]
 };
-console.log(Quiz.data[1].options[0]);
-// var randomQuestion = Quiz.data[Math.round(Math.random())];
-// console.log(randomQuestion);
+// Declaring Global variables
+var click =0;
+// var flag =true;
+var intervalId;
 var ansClicked;
-var count = 25;
+var count = 30;
 var name ;
 var index = 0;
 var correctAns = 0;
@@ -32,152 +37,44 @@ var wrongAns = 0;
 var notAns = 0;
 var randomQuestion = Quiz.data[index];
 var answer = Quiz.data[index].ans;
-console.log(answer);
-function initialScreen(){
-	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
-	$(".mainArea").html(startScreen);
-  // $(".container").html("start");
-  // startText = "<p> start</p>";
-  var text = $("<p>");
-  text.html("<h1>Start Quiz</h1>");
-  $(".mainArea").append(text);
-  // $(".mainArea").html(startText);
-	$(".container").hide();
-}
+
+// To start the game
+$(document).ready(function(){
+// calling the start screen
 initialScreen();
-
+// To start when start buton is clicked
 $("body").on("click", ".start-button", function(event){
+    audio.pause();
+    clickSound.play();
 	event.preventDefault();  // added line to test issue on GitHub Viewer
-	// clickSound.play();
-	
+  // Showing the container containg quiz questions
     $(".container").show();
+  // Hiding the initial screen
     $(".mainArea").hide();
-    // run();;
-       // var qyes = randomQuestion.question[index];
-       // var randomQuestion = Quiz.data[index];/
-       createQuestion(randomQuestion.question[index]);
-       // createQuestion(randomQuestion);
-       decrement() ;
-       stop();
-       startScreen.hide();
- 
-}); // Closes start-button click
-function run() {
-      intervalId = setInterval(decrement, 1000);
+  // Starting the timer
+      run();
+  // Calling function to create questions
       createQuestion(randomQuestion);
-    }
-    //  The decrement function.
-    function decrement() {
-      //  Decrease number by ne.
-      count--;
-      //  Show the number in the #show-number tag.
-      $("#nw").html("<h2>Time Remmaining" +":"+""+ count +""+"seconds</h2>");
-      //  Once number hits zero...
-      if (count === 0) {
-        //  ...run the stop function.
-        stop();
-      }
-    }
-    //  The stop function
-    function stop() {
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-
-      clearInterval(intervalId);
-      //  Alert the user that time is up.
-        // alert("Time Up!");
-      	count = 25;
-      	index = index+1;
-
-      	console.log(Quiz.data.length );
-      	console.log(index);
-
-      	if(Quiz.data.length > index){
-      		console.log("Inside.....");
-      	run();
-      	$("#option").empty();
-      	$("#ques").empty();
-      	$("#nw").empty();
-        answer = Quiz.data[index].ans;
-        console.log(answer);
-      	var randomQuestion = Quiz.data[index];
-      	createQuestion(randomQuestion);
-      	console.log(index);
-      	
-      }else{
-      	$("#option").empty();
-      	$("#ques").empty();
-      	$("#nw").empty();
-
-      	$("#ans").text("Number of correct Answers"+""+ ":"+""+correctAns);
-      	var wrong  = $("<div>");
-        console.log(answer);
-      	wrong.html("Number of Wrong Answers"+""+ ":"+""+wrongAns+"");
-      	$("#ans").append(wrong);	
-      	var noAns = $("<div>");
-      	noAns.html("Not Answered"+""+ ":"+""+notAns);
-      	$("#ans").append(noAns);
-        var btn = $("<button>");
-        btn.attr('data-name',"reset");
-        btn.text("RESET");
-        $("#reset").append(btn);
-        // index = 0;
-        // listen for reset button click
-      $("#reset").on("click", function() {
-        // createQuestion(randomQuestion[index]);
-         // location.href=location.href;
-          createQuestion(Quiz.data);
-       // createQuestion(randomQuestion);
+  // Decrementing the value of counter 
        decrement() ;
-       stop();
-       startScreen.hide();
-  }); 
-      	}
-    }
-   console.log(index); 
-    //  Execute the run function.
-    run();
-
-
-function createQuestion(randomQuestion){
-
-  $("#ques").html(randomQuestion.question);
-
-  for(var i=0;i<randomQuestion.options.length;i++){
-  	var choices = $("<li>");
-  	choices.html(randomQuestion.options[i]);
-  	$("#option").append(choices);
-  	 console.log(randomQuestion.options[i]);
-
-
-	}
-	 // clearInterval(intervalId);
-}
+      
+}); // Closes document.ready function
+// Things to be done when the 1of 4 options are pressed
 
 $('body').on("click", 'li',function(event){
-	// $('li').click(function(){
-		// alert("Hi");
-		
-		// alert("hi");
+     audio.pause();
+     clickSound.play();
 		var value = $(this).text();
 		var answer = Quiz.data[index].ans;
-console.log(value);
-		 // index = index+1;
-		console.log(answer);
-       	// console.log(value);
+    // if user click equals to the answer the increment the current count and stop the function
        	if(value===answer){
-       		console.log("correct");
        		correctAns++;
-          // console.log(Quiz.data[index].images);
-          timerSet();
-       		
-          // $("#picture").hide();
+          timerSetWon();
        	}
        	else if(value!=answer){
-       		console.log("wrong answer");
        		wrongAns++;
-       		stop();
+          timerSetLoose();
+          
        	}
        	else{
        		console.log("Not Answered");
@@ -186,20 +83,119 @@ console.log(value);
       
 	});
 
-function timerSet(){
-          $("#picture").html("correct");   
+});
+
+
+// Functions to be performed to the starting screen
+function initialScreen(){
+  startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+  $(".mainArea").html(startScreen);
+  var text = $("<p>");
+  text.html("<h1>Trivia Game Quiz</h1>");
+  $(".mainArea").append(text);
+  // $(".mainArea").html(startText);
+  $(".container").hide();
+}
+// To set the timer if player has won
+function timerSetWon(){
+          $("#picture").html("<h1>Correct Answer</h1>");   
           var pic = $("<img>");
           pic.attr("src",Quiz.data[index].images);
-          // pic.html(randomQuestion.images);
-          // pic.html()
-          console.log(randomQuestion.images);
-
           $("#picture").append(pic);
-          // $("#picture").html("<img" +randomQuestion.images+"/>");
           console.log("correctansis"+""+correctAns);
            var timeoutID = window.setTimeout(function(){
               $("#picture").html("");
               stop();
            }, [5000]);    
 }
-});
+
+// To set the timer if player has Lost
+function timerSetLoose(){
+          $("#picture").html("<h1>Wrong Answer</h1>");   
+          var annsw = $("<div>");
+          annsw.html("<h1>")
+          $("#picture").html("<h1>correct answer is"+""+Quiz.data[index].ans+"</h1>");
+           var timeoutID = window.setTimeout(function(){
+              $("#picture").html("");
+              stop();
+           }, [5000]);    
+}
+// To create question and answers
+function createQuestion(randomQuestion){
+   audio.play();
+  $("#ques").html(randomQuestion.question);
+  for(var i=0;i<randomQuestion.options.length;i++){
+    var choices = $("<li>");
+    choices.html(randomQuestion.options[i]);
+    $("#option").append(choices);
+     console.log(randomQuestion.options[i]);
+  }
+
+}
+// To start the timer
+function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
+//  The decrement function.
+    function decrement() {
+//  Decrease count by 1.
+      count--;
+//  Show the number in the #show-number tag.
+      $("#nw").html("<h2>Time Remmaining" +":"+""+ count +""+"seconds</h2>");
+//  Once number hits zero...
+      if (count === 0) {
+//  ...run the stop function.
+        stop();
+      }
+    }
+//  The stop function
+    function stop() {
+//  Clears our intervalId
+//  We just pass the name of the interval
+//  to the clearInterval function.
+      clearInterval(intervalId);
+// Reset the counter
+        count = 30;
+// increment the index     
+        index = index+1;
+// if lenegth of array is greater than the value of index change the question as the timer tends to 0
+        if(Quiz.data.length > index){
+        run();
+        $("#option").empty();
+        $("#ques").empty();
+        $("#nw").empty();
+        answer = Quiz.data[index].ans;
+        var randomQuestion = Quiz.data[index];
+        createQuestion(randomQuestion);        
+      }else{
+        $("#option").empty();
+        $("#ques").empty();
+        $("#nw").empty();
+
+        $("#ans").html("<h3>Number of correct Answers" +""+ ":"+""+correctAns+"</h3>");
+        var wrong  = $("<div>");
+        console.log(answer);
+        wrong.html("<h3>Number of Wrong Answers"+""+ ":"+""+wrongAns+"</h3>");
+        $("#ans").append(wrong);  
+        var noAns = $("<div>");
+        noAns.html("<h3>Not Answered"+""+ ":"+""+notAns+"</h3");
+        $("#ans").append(noAns);
+    // Creating Reset button
+        var btn = $("<button>");
+        btn.attr('data-name',"reset");
+        btn.text("RESET");
+        $("#reset").append(btn);
+        // listen for reset button click
+      $("#reset").on("click", function() {
+        index = 0;
+        randomQuestion = Quiz.data[index];
+        answer = Quiz.data[index].ans;
+        run();
+        createQuestion(randomQuestion);
+        decrement();
+        $("#ans").empty();
+        $("#reset").hide();
+
+  }); 
+        }
+}
